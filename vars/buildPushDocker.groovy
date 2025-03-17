488387e8393d475874b7ed139577ccb5fb14f112
Ali Sharb <111139523+aliSharb@users.noreply.github.com>
2025-03-17 17:09:31 +0200

    Update buildPushDocker.groovy

--- vars/buildPushDocker.groovy
+++ vars/buildPushDocker.groovy
@@ -1,16 +1,21 @@
-def call(String dockerImage, String dockerContext, String credentialsId) { 
+def call(String dockerImage, String dockerContext, String credentialsId) {
     script {
         stage('Build Docker Image') {
+            echo "🔨 Building Docker image: ${dockerImage}..."
             sh "docker build -t ${dockerImage} ${dockerContext}"
         }
+
         stage('Push Docker Image to Docker Hub') {
+            echo "📤 Pushing Docker image: ${dockerImage}..."
             withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
                 sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
                 sh "docker push ${dockerImage}"
             }
         }
+
         stage('Clean Up Local Docker Image') {
-            sh "docker rmi ${dockerImage}"
+            echo "🧹 Cleaning up local Docker image..."
+            sh "docker rmi ${dockerImage} || true"
         }
     }
 }
