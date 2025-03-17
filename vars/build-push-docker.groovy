bbf44192d8512adefd322593994027203add39e0
Ali Sharb <111139523+aliSharb@users.noreply.github.com>
2025-03-17 14:52:10 +0200

    Update build-push-docker.groovy

--- vars/build-push-docker.groovy
+++ vars/build-push-docker.groovy
@@ -4,7 +4,6 @@ def call(String dockerImage, String dockerContext, String credentialsId) {
             sh "docker build -t ${dockerImage} ${dockerContext}"
         }
     }
-
     stage('Push Docker Image to Docker Hub') {
         script {
             withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
@@ -13,7 +12,6 @@ def call(String dockerImage, String dockerContext, String credentialsId) {
             }
         }
     }
-
     stage('Clean Up Local Docker Image') {
         script {
             sh "docker rmi ${dockerImage}"
